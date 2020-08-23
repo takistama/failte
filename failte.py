78d27b9a6f351a4f5b82cbdcd188999ad55850eb
ExhaustedBiped <64213772+ExhaustedBiped@users.noreply.github.com>
2020-08-23 13:41:45 -0400

    Channel Name Highlight
    Wasn't highlighting channel names due to special formatting.

--- failte.py
+++ failte.py
@@ -19,7 +19,7 @@ async def on_member_join(member):
         try:
             embed = discord.Embed(colour = discord.Colour.green())
             embed.set_author(name = member.name,icon_url = member.avatar_url)
-            embed.add_field(name = "Welcome",value = f"**Hello,{member.mention}! Welcome to {member.guild.name}\nCheck out #info and #rules !**",inline = False)
+            embed.add_field(name = "Welcome",value = f"**Hello,{member.mention}! Welcome to {member.guild.name}\nCheck out** #info and #rules !",inline = False)
             embed.set_thumbnail(url = member.guild.icon_url)
             await channel.send(embed = embed)
         except Exception as e:

cc391923d2e6d9ff956ea8ea4ce3e0bc2b930876
ExhaustedBiped <64213772+ExhaustedBiped@users.noreply.github.com>
2020-08-23 13:13:17 -0400

    First Post
    + Failte
    + requirements

---
+++ failte.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext.commands import Bot
+
+BOT_PREFIX = "f-"
+BOT_TOKEN = "token"
+WELCOME_CHANNEL_ID = ID
+
+failte = Bot(command_prefix = BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    print("Logged in as "+failte.user.name)
+
+@bot.event
+async def on_member_join(member):
+    await failte.wait_until_ready()
+    try:
+        channel = failte.get_channel(WELCOME_CHANNEL_ID)
+        try:
+            embed = discord.Embed(colour = discord.Colour.green())
+            embed.set_author(name = member.name,icon_url = member.avatar_url)
+            embed.add_field(name = "Welcome",value = f"**Hello,{member.mention}! Welcome to {member.guild.name}\nCheck out #info and #rules !**",inline = False)
+            embed.set_thumbnail(url = member.guild.icon_url)
+            await channel.send(embed = embed)
+        except Exception as e:
+            raise e
+    except Exception as e:
+        raise e
+
+
+
+failte.run(BOT_TOKEN)
